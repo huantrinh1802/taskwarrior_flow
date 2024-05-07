5255443b84bade68aff466be69ab258cf80b3483
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2024-05-07 12:37:02 +1000

    feat: Enhanced testing environment and tooling configuration
    - Added `dateparser` dev dependency to `pyproject.toml` and corresponding types
    - Configured pytest options in `pyproject.toml` for better output during tests
    - Created pytest configuration file `pytest.ini` in tests directory
    - Added new test methods in `test_cli.py`
    - Created utilities for simulated input during tests in `utils.py`
    - Created scaffold for new test cases in `test_utily_commands` directory
    - Initiated use of fixtures in test cases for reusability

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,12 +1,5 @@
-from typer.testing import CliRunner
-
-from tools.main import app
-
-runner = CliRunner()
-
-
-def test_app():
-    result = runner.invoke(app, ["--help"])
+def test_app(test_app):
+    result = test_app["runner"].invoke(test_app["app"], ["--help"])
     assert result.exit_code == 0
     assert "task" in result.stdout
     assert "utils" in result.stdout
