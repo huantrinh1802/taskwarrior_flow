318e23d4c77c69034c17711cd96e793819768a92
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2024-05-08 16:29:36 +1000

    feat: Additional configuration options for test_app
    - Imported json and os modules for configuration file management
    - Added option to use environment-defined configuration file path or default one
    - Added writing of mock configuration data to a file
    - Mock data includes additional templates for recurring tasks and bills
    - Expanded saved queries section for tailored usage during testing
    
    [ooo]

--- tests/test_utily_commands/test_view/test_template.py
+++ tests/test_utily_commands/test_view/test_template.py
@@ -4,5 +4,4 @@ from tests import utils
 def test_view_template(test_app):
     utils.pipe_input('\r')
     result = test_app["runner"].invoke(test_app["app"], ["utils", "view"])
-    print(result.stdout)
     assert result.exit_code == 0
